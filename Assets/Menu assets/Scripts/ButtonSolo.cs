ad002179fc64255e6262aefc3d9388d58be4bfb2
Guillaume Birame Gomis <guillaumebiramegomis@gmail.com>
2023-12-19 12:18:36 +0000

    Initial commit

---
+++ Assets/Menu assets/Scripts/ButtonSolo.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonSolo : MonoBehaviour
+{
+
+    public void LoadScene(string scene_name)
+    {
+        SceneManager.LoadScene(scene_name);
+    }
+
+}
